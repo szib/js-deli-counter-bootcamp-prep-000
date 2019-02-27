07e133a82e3e4a796e6d82649087ebe683aed90e
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 23:23:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function takeANumber(line, name) {
 
 function nowServing(line) {
   if (line.length === 0) return 'There is nobody waiting to be served!'
-  return line.shift()
+  return `Currently serving ${line.shift()}.`
 }
