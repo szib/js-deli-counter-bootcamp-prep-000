8170252655d39ca3dbab84aa5a8797f5c71a7398
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 23:20:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(line, name) {
-  console.log(line)
-  line = line.push(name)
-  console.log(line)
+  // console.log(line)
+  line.push(name)
+  // console.log(line)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
